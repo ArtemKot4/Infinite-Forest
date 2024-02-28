673e5586d18deb0703ea4d91064460514accc056
Artem0n4 <artemon4xxl@gmail.com>
2024-02-28 14:29:05 +0300

    Действительно 🌚 Спасибо Макс!

--- typescript/items/crystals.ts
+++ typescript/items/crystals.ts
@@ -20,9 +20,9 @@ const changeTorchSpecial = (special: "iced" | "flamed", coords}) => {
  if(block.id === BlockID["eucalyptus_torch"]) { minus(Player.getLocal(), item.id, item.count - 1, 0);
  const place = coords.relative;
  for(let i = 0; i <= 10; i++){
- spawnParticle(smoke, coords.x, coords.y + 0.8, coords.z, 0, 0.3, 0)
+ spawnParticle(smoke, coords.x, coords.y + 0.8, coords.z, 0, 0.3, 0); };
      region.setBlock(coords.x, coords.y, coords.z, BlockID[special + "_eucalyptus_torch"],0)
- }
+ 
  }
 }
  
