57f78bb6888762132da08fb50d5c6f3cf613e4c6
ArtemOn456 <artemon4xxl@gmail.com>
2024-06-25 00:45:35 +0300

    .

--- code/api/amulet/Amulet.ts
+++ code/api/amulet/Amulet.ts
@@ -1,3 +1,36 @@
+interface IAmuletUIPosition {
+    x: int,
+    y: int,
+};
+
 class Amulet {
-    
+    public static list: Record<string, int>[] = []
+  public ITEM: FItem;
+  constructor(public name: string, public texture: string, public slot: IAmuletUIPosition) {
+    this.ITEM = new FItem(
+      name,
+      1,
+      "item.infinite_forest.amulet." + name,
+      texture,
+      0
+    );
+    this.drawUIElement();
+    Amulet.list.push({[name]: this.ITEM.getID()});
+  };
+  public drawUIElement() {
+    AmuletUI.UI.content.elements[this.name + "_button"] = {
+        type: "button",
+        scale: 2,
+        x: this.slot.x,
+        y: this.slot.y,
+        bitmap: "amulet_button",
+        bitmap2: "amulet_button_pressed"
+    };
+    AmuletUI.UI.content.drawing.push({
+        type: "bitmap",
+        x: this.slot.x * 1.5,
+        y: this.slot.y * 0.5,
+        bitmap: this.texture
+    })
+  }
 }
--- code/api/amulet/AmuletUI.ts
+++ code/api/amulet/AmuletUI.ts
@@ -1,12 +1,30 @@
-class AmuletUI {
-  public readonly container = new UI.Container();
-  public static UI = new UI.Window({
+abstract class AmuletUI {
+  public static content: UI.WindowContent = {
     drawing: [
       {
         type: "background",
         color: android.graphics.Color.argb(0.3, 56 / 256, 65 / 256, 56 / 256)
       }
     ],
-  });
+    elements: {
+      
+    }
+  }
+  public static UI: UI.Window;
+
+  static {
+    AmuletUI.UI = new UI.Window(AmuletUI.content as UI.WindowContent);
+  };
+
+  public static detectAmulets(player: int) {
+    const actor = new PlayerActor(player);
+    for(let i = 0; i <= 35; i++) {
+    const slot = actor.getInventorySlot(i);
+    //TODO: continue
+    }
+  }
+  public static redraw(player: int) {
+
+  }
 };
 
