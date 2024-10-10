a8b348d82fa6f5a451bba63ead7e79a90ac68b60
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-10 10:18:14 +0300

    Свечи становятся всё лучше
    Очень интересные вещи были добавлены, только вы о них не узнаете, хоть и хочется рассказать. Я тоже жук

--- code/blocks/candle/Candle.ts
+++ code/blocks/candle/Candle.ts
@@ -1,6 +1,6 @@
 class Candle extends FBlock {
   public static readonly CANDLE_MAX = 5;
-
+  public static readonly CANDLE_MAX_DATA = 4;
   public static meshes = (() => {
     const one = new RenderMesh();
     one.importFromFile(MODELSDIR + "block/candle_max.obj", "obj",{
@@ -42,7 +42,7 @@ class Candle extends FBlock {
     return [one, two, three, four, five];
   })();
 
-  constructor(public id: string, level: int) {
+  constructor(public id: string, public lightLevel: int) {
     super(
       id,
       [
@@ -57,23 +57,28 @@ class Candle extends FBlock {
     Block.registerClickFunction(id, (coords, item, block, player) => {
       const region = BlockSource.getDefaultForActor(player);
 
-      if (item.id === block.id && block.data < Candle.CANDLE_MAX) {
+      if (item.id === block.id && block.data < Candle.CANDLE_MAX_DATA) {
         region.setBlock(coords.x, coords.y, coords.z, block.id, block.data + 1);
+        return
       }
 
       if (item.id === VanillaItemID.flint_and_steel) {
-        if (block.id === BlockID["candle_lit_4"]) {
+        if (block.id === ELightCandes.twenty.getID()) {
           return;
         }
-
+     
         let newID = "candle_lit_1";
 
-        const stringId = String(IDRegistry.getIdInfo(block.id).split(":")[1]);
+        if(block.id !== BlockID["candle_unlit"]) {
+
+        const stringId = String(IDRegistry.getIdInfo(block.id)).split(":")[1];
         const endChar = Number(stringId[stringId.length-1]);
 
-        if (typeof endChar === "number") {
+        if (typeof endChar === "number" && (endChar < Candle.CANDLE_MAX_DATA)) {
+
           newID = "candle_lit_" + (endChar + 1);
         }
+      };
 
         Entity.setCarriedItem(
           player,
@@ -95,19 +100,23 @@ class Candle extends FBlock {
       }
     });
 
-    BlockRegistry.setLightLevel(id, level);
-
     Candle.meshes.forEach((v, i) => this.setupBlockModelFromMesh(v, i));
   }
 
-  public static getCount(region: BlockSource, coords: Vector) {
+  public create(): this {
+    super.create();
+    BlockRegistry.setLightLevel(BlockID[this.getID()], this.lightLevel);
+    return this;
+  }
+
+  public static getCount(region: BlockSource | WorldRegion, coords: Vector) {
     const block = region.getBlock(coords.x, coords.y, coords.z);
-    return Number(block.id === BlockID["candle"] ? block.data : 0);
+    return Number(block.id === BlockID["candle"] ? block.data + 1 : 1);
   }
 }
 
 namespace ELightCandes {
-  export const none = new Candle("unlit_candle", 0).create();
+  export const none = new Candle("candle_unlit", 0).create();
   export const three = new Candle("candle_lit_1", 3).create();
   export const six = new Candle("candle_lit_2", 6).create();
   export const nine = new Candle("candle_lit_3", 9).create();
@@ -120,7 +129,7 @@ Translation.addTranslation("block.infinite_forest.candle", {
 });
 
 class CandleTileReplacer {
-  public static coordsList = new Set();
+  public static coordsList = new Set<Vector>();
   public static initialize(x: int, y: int, z: int) {
     if(this.coordsList.has({x, y, z})) {
         return;
@@ -138,19 +147,19 @@ class CandleTileReplacer {
         const region = BlockSource.getCurrentClientRegion();
         const block = region.getBlock(this.x, this.y, this.z);
         if (World.getThreadTime() % 40 === 0) {
-          if (!IDRegistry.getNameByID(block.id).startsWith("candle_lit")) {
+          if (!String(IDRegistry.getNameByID(block.id)).startsWith("candle_lit")) {
             this.remove = true;
           }
         }
-        if (World.getThreadTime() % 15 === 0) {
+        if (World.getThreadTime() % 5 === 0) {
           block.data >= 0 &&
             Particles.addParticle(
               EParticleType.FLAME,
               this.x + 0.5,
-              this.y + 0.95,
+              this.y + 0.75,
               this.z + 0.5,
               0,
-              0.05,
+              0.005,
               0
             );
 
@@ -158,10 +167,10 @@ class CandleTileReplacer {
             Particles.addParticle(
               EParticleType.FLAME,
               this.x + 0.8,
-              this.y + 0.85,
+              this.y + 0.65,
               this.z + 0.5,
               0,
-              0.05,
+              0.005,
               0
             );
 
@@ -169,10 +178,10 @@ class CandleTileReplacer {
             Particles.addParticle(
               EParticleType.FLAME,
               this.x + 0.2,
-              this.y + 0.85,
+              this.y + 0.65,
               this.z + 0.5,
               0,
-              0.05,
+              0.005,
               0
             );
 
@@ -180,10 +189,10 @@ class CandleTileReplacer {
             Particles.addParticle(
               EParticleType.FLAME,
               this.x + 0.5,
-              this.y + 0.85,
+              this.y + 0.65,
               this.z + 0.8,
               0,
-              0.05,
+              0.005,
               0
             );
 
@@ -191,14 +200,18 @@ class CandleTileReplacer {
             Particles.addParticle(
               EParticleType.FLAME,
               this.x + 0.5,
-              this.y + 0.85,
+              this.y + 0.65,
               this.z + 0.2,
               0,
-              0.05,
+              0.005,
               0
             );
         }
       },
     });
+  };
+  
+  public static isCandleFlamed(x: int, y: int, z: int) {
+    return CandleTileReplacer.coordsList.has({x, y, z});
   }
 }
