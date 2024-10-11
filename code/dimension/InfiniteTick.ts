af2da07344313cabd853e841acef9147b4eb84e6
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-11 16:37:50 +0300

    Подписывайтесь на проклятия! Всё интересное впереди

--- code/dimension/InfiniteTick.ts
+++ code/dimension/InfiniteTick.ts
@@ -1,50 +1,50 @@
-
-
-const InfiniteTick = () => {
-  return {
-    update(): void {
-      if (Player.getDimension() !== InfiniteForest.id) {
-        this.remove = true;
-      };
-
-      const pos = Player.getPosition();
-      const biome = World.getBiome(pos.x, pos.z);
-      const player = Player.getLocal();
-
-      if (biome === ForestBiomes.FirefliesForest.id) {
-        if (World.getThreadTime() % 15 === 0) {
-          addFire(pos);
-        };
-
-        addGlowworm(pos, EForestParticle.GLOWWORM_1);
-        addGlowworm(pos, EForestParticle.GLOWWORM_2);
-        addGlowworm(pos, EForestParticle.GLOWWORM_4);
-      };
-
-      if (biome === ForestBiomes.WinterForest.id) {
-        ColdCurse.runSnow(pos.x, pos.y + 12.5, pos.z, 16, 16); //32 24
-        ColdCurse.allowHas(player) && Entity.damageEntity(player, 1);
-      };
-
-      if (World.getThreadTime() % 30 === 0) {
-        ColdCurse.sendMessage(pos);
-      };
-
-      if (pos.y >= 100) {
-
-        if (pos.y <= 130) {
-
-          ColdCurse.runSnow(pos.x, 130, pos.z, 512);
-        } else {
-          
-          //ColdCurse.onTick(this.ticker, player);
-          ColdCurse.runSnow(pos.x, pos.y + 12.5, pos.z, 64, 24);
-        }
-        return;
-      }
-    }
+Callback.addCallback("LocalTick", () => {
+  if (Player.getDimension() !== InfiniteForest.id) {
+    return;
   };
-};
+
+  const pos = Player.getPosition();
+  const biome = World.getBiome(pos.x, pos.z);
+  const player = Player.getLocal();
+
+  if (biome === ForestBiomes.FirefliesForest.id) {
+    if (World.getThreadTime() % 15 === 0) {
+      addFire(pos);
+    };
+
+    addGlowworm(pos, EForestParticle.GLOWWORM_1);
+    addGlowworm(pos, EForestParticle.GLOWWORM_2);
+    addGlowworm(pos, EForestParticle.GLOWWORM_4);
+  };
+
+  if (biome === ForestBiomes.WinterForest.id) {
+    ColdCurse.runSnow(pos.x, pos.y + 12.5, pos.z, 16, 16); //32 24
+    ColdCurse.allowHas(player) && Entity.damageEntity(player, 1);
+  };
+
+  if (World.getThreadTime() % 30 === 0) {
+    ColdCurse.sendMessage(pos);
+  };
+
+  ColdCurse.subscribe(() => {
+    if (pos.y >= 100) {
+
+      if (pos.y <= 130) {
+  
+        ColdCurse.runSnow(pos.x, 130, pos.z, 512);
+      } else {
+        
+        ColdCurse.runSnow(pos.x, pos.y + 12.5, pos.z, 64, 24);
+        Entity.damageEntity(player, 2);
+        
+      }
+      return;
+    }
+  })
+  return;
+})
+   
+
 
 Callback.addCallback("PlayerChangedDimension", function (player, from, to) {
   if (Entity.getDimension(player) === InfiniteForest.id) {
@@ -55,7 +55,7 @@ Callback.addCallback("PlayerChangedDimension", function (player, from, to) {
     //   "forest_title",
     //   ["crystal", "snow", "fire", "forest"]
     // );
-    Updatable.addLocalUpdatable(InfiniteTick());
+    
   }
 });
 
