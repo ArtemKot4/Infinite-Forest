6368a74f58e6ca791ef2f37a0a4aad8394a8466d
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-12 14:58:44 +0300

    Супер пупер дупер мега класс топ 1 пушка огонь вспышка обёртка апи для тегов

--- code/tag_register.ts
+++ code/tag_register.ts
@@ -1,3 +1,33 @@
-const candle = TagRegistry.getOrCreateGroup<int>("candle");
-candle.addCommonObject(ELightCandles.none.getID());
-candle.addCommonObject("string");
+class Tag {
+  public group: TagRegistry.TagGroup<int | int[]>;
+  constructor(name: string) {
+    this.group = TagRegistry.getOrCreateGroup<int>(name);
+  }
+  public addItem(id: int, tags?: string[]) {
+    this.group.addCommonObject(id, ...tags);
+  }
+  public addBlock(id: int, tags?: string[]): void;
+  public addBlock(id: int, data: int): void;
+  public addBlock(id: int, data: int, tags?: string[]): void;
+  public addBlock(id: int, data: int | string[], tags?: string[]) {
+    let obj: any = id;
+
+    if (Array.isArray(data)) {
+
+      this.group.addCommonObject(obj, ...data);
+
+    } else {
+
+      if (data) {
+        obj = [id, data];
+      };
+
+      this.group.addCommonObject(obj, ...tags);
+    }
+  }
+}
+
+namespace Tags {
+    export const Candle = new Tag("candle");
+    Candle.addBlock(ELightCandles.none.getID());
+}
